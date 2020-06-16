6a09f11434f5877dd9829add3cd0668bc7a527f0
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-06-16 12:54:52 +0530

    Python variants and .bat scripts

---
+++ post-link.bat
@@ -0,0 +1,3 @@
+%CONDA_PREFIX/Scripts/pip install orderedattrdict\>=1.4.3 xmljson\>=0.1.5 cron-descriptor tables
+%CONDA_PREFIX/Scripts/yarn config set ignore-engines true
+%CONDA_PREFIX/Scripts/gramex setup --all
--- post-link.sh
+++ post-link.sh
@@ -1,2 +1,3 @@
-$CONDA_PREFIX/bin/pip install orderedattrdict>=1.4.3 xmljson>=0.1.5 cron-descriptor tables
+$CONDA_PREFIX/bin/pip install orderedattrdict\>=1.4.3 xmljson\>=0.1.5 cron-descriptor tables
+$CONDA_PREFIX/bin/yarn config set ignore-engines true
 $CONDA_PREFIX/bin/gramex setup --all
