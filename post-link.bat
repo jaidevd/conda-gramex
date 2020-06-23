c8bb790826a6726565cc6345e0fa8604c597be53
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-06-23 08:48:54 +0530

    Update package versions, fix installed modules

--- post-link.bat
+++ post-link.bat
@@ -1,3 +1,4 @@
-%CONDA_PREFIX/Scripts/pip install orderedattrdict\>=1.4.3 xmljson\>=0.1.5 cron-descriptor tables
-%CONDA_PREFIX/Scripts/yarn config set ignore-engines true
-%CONDA_PREFIX/Scripts/gramex setup --all
+%PREFIX%\Scripts\pip.exe install orderedattrdict>=1.4.3 xmljson>=0.1.5 cron-descriptor tables
+call %PREFIX%\Library\bin\yarn.cmd config set ignore-engines true
+%PREFIX%\Scripts\gramex.exe setup --all
+if errorlevel 1 exit 1
