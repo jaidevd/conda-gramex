84a20b33bc325262b21fa5532d1a7e3632ed507c
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-06-23 11:14:19 +0530

    Explicit installation of some apps; skip NLG on windows

--- post-link.bat
+++ post-link.bat
@@ -1,4 +1,12 @@
 %PREFIX%\Scripts\pip.exe install orderedattrdict>=1.4.3 xmljson>=0.1.5 cron-descriptor tables
 call %PREFIX%\Library\bin\yarn.cmd config set ignore-engines true
-%PREFIX%\Scripts\gramex.exe setup --all
+%PREFIX%\Scripts\gramex.exe setup ui
+%PREFIX%\Scripts\gramex.exe setup admin
+%PREFIX%\Scripts\gramex.exe setup admin2
+%PREFIX%\Scripts\gramex.exe setup capture
+%PREFIX%\Scripts\gramex.exe setup filemanager
+%PREFIX%\Scripts\gramex.exe setup logviewer
+%PREFIX%\Scripts\gramex.exe setup pynode
+%PREFIX%\Scripts\gramex.exe setup languagetool
+%PREFIX%\Scripts\gramex.exe setup configeditor
 if errorlevel 1 exit 1
