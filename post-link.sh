885c860bcd1e64b72ea8a53736d85096a4eaef77
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-07-20 12:08:57 +0530

    Move RPy2 to pip, r-base and r-essentials to conda

--- post-link.sh
+++ post-link.sh
@@ -4,7 +4,7 @@ $CONDA_PREFIX/bin/pip install crontab>=0.21 cssselect diskcache>=2.8.3 inflect i
 $CONDA_PREFIX/bin/pip install ldap3>=2.2.4 markdown oauthlib>=1.1.2 passlib>=1.6.5 pathlib
 $CONDA_PREFIX/bin/pip install pathtools>=0.1.1 psutil pymysql python-pptx>=0.6.6 pyyaml>=5.1
 $CONDA_PREFIX/bin/pip install redis>=2.10.0 selenium six sqlitedict>=1.5.0 textblob
-$CONDA_PREFIX/bin/pip install tornado==5.1.1 watchdog>=0.8 xlrd
+$CONDA_PREFIX/bin/pip install tornado==5.1.1 watchdog>=0.8 xlrd rpy2
 
 $CONDA_PREFIX/bin/yarn config set ignore-engines true
 $CONDA_PREFIX/bin/gramex setup --all
