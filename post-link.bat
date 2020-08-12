160617d148c13355e420edadfd6b430c3501bd25
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-08-12 09:40:52 +0530

    WIP

--- post-link.bat
+++ post-link.bat
@@ -9,13 +9,5 @@
 %PREFIX%\Scripts\pip.exe install tornado ==5.1.1 watchdog>=0.8 xlrd rpy2
 
 call %PREFIX%\Library\bin\yarn.cmd config set ignore-engines true
-%PREFIX%\Scripts\gramex.exe setup ui
-%PREFIX%\Scripts\gramex.exe setup admin
-%PREFIX%\Scripts\gramex.exe setup admin2
-%PREFIX%\Scripts\gramex.exe setup capture
-%PREFIX%\Scripts\gramex.exe setup filemanager
-%PREFIX%\Scripts\gramex.exe setup logviewer
-%PREFIX%\Scripts\gramex.exe setup pynode
-%PREFIX%\Scripts\gramex.exe setup languagetool
-%PREFIX%\Scripts\gramex.exe setup configeditor
+%PREFIX%\Scripts\gramex.exe setup --all
 if errorlevel 1 exit 1
