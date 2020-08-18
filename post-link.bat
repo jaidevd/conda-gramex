ba064f3d94b572ef7542cf84a70fcbfdd9131cdd
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2020-08-18 07:49:59 +0530

    WIP

--- post-link.bat
+++ post-link.bat
@@ -1,5 +1,3 @@
-%PREFIX%\Scripts\conda.exe install -c default rpy2 r-rmarkdown r-ggplot2 -y
-
 %PREFIX%\Scripts\pip.exe install orderedattrdict>=1.4.3 xmljson>=0.1.5 cron-descriptor tables
 %PREFIX%\Scripts\pip.exe install argh>=0.24.1 boto3>=1.5 cachetools>=3.0.0 colorlog>=2.7.0
 %PREFIX%\Scripts\pip.exe install crontab>=0.21 cssselect diskcache>=2.8.3 inflect ipdb jmespath
@@ -7,6 +5,8 @@
 %PREFIX%\Scripts\pip.exe install pathtools>=0.1.1 psutil pymysql python-pptx>=0.6.6 pyyaml>=5.1
 %PREFIX%\Scripts\pip.exe install redis>=2.10.0 selenium six sqlitedict>=1.5.0 textblob
 %PREFIX%\Scripts\pip.exe install tornado ==5.1.1 watchdog>=0.8 xlrd rpy2
+
+%PREFIX%\Scripts\conda.exe install -c default r-rmarkdown r-ggplot2 -y
 
 call %PREFIX%\Library\bin\yarn.cmd config set ignore-engines true
 %PREFIX%\Scripts\gramex.exe setup --all
